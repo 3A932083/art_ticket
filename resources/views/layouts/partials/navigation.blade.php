b9ab8aacf6dc1206b651b644cea6041aecc15d07
jiang-shu-yi <3a932083@gm.student.ncut.edu.tw>
2022-11-22 21:06:04 +0800

    展覽、體驗、講座的頁面route

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -1,15 +1,15 @@
 <!-- Responsive navbar-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
     <div class="container px-lg-5">
-        <a class="navbar-brand" href="#!">AT.</a>
+        <a class="navbar-brand" href="{{route('home.new')}}">AT.</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse " id="navbarSupportedContent">
           <div class="container">
               <div class="row ">
                   <div class="col-10 nav" style="color: #fefefe">
-                      <a class="nav-link link-light" aria-current="page" href="#!">展覽</a>
-                      <a class="nav-link link-light" href="#!">體驗</a>
-                      <a class="nav-link link-light" href="#!">講座</a>
+                      <a class="nav-link link-light" aria-current="page" href="{{route('home.show')}}">展覽</a>
+                      <a class="nav-link link-light" href="{{route('home.diy')}}">體驗</a>
+                      <a class="nav-link link-light" href="{{route('home.lecture')}}">講座</a>
                   </div>
                   <div class="col" style="color: #fefefe">
                       <a class="nav-link link-light text-end" href="#!">會員</a>
