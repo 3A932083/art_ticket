83255d67e3d0d1cf210d7824088b0a75e6529d57
Chen Yi-Hsuan <s3a932087@student.ncut.edu.tw>
2022-11-30 15:29:09 +0800

    新增events資料表

---
+++ database/migrations/2022_11_30_070650_creat_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('activity_id');
+            $table->foreign('activity_id')->references('id')->on('activities');
+            $table->dateTime('time');
+            $table->integer('count');
+            $table->integer('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
